47d1668f0059a72e12b82892b9f3c854f5551e3e
Thomas Deal <justarandomgeek@gmail.com>
2017-09-22 20:10:27 -0400

    Self-Sealing Stem Bolts

---
+++ prototypes/items.lua
@@ -0,0 +1,20 @@
+data:extend{
+  {
+    type = "item",
+    name = "self-sealing-stem-bolt",
+    icon = "__selfsealingstembolts__/graphics/self-sealing-stem-bolt.png",
+    flags = {},
+    subgroup = "intermediate-product",
+    order = "z[lol]-a[self-sealing-stem-bolt]",
+    stack_size = 50,
+  },
+  {
+    type = "item",
+    name = "reverse-ratcheting-routing-planer",
+    icon = "__selfsealingstembolts__/graphics/reverse-ratcheting-routing-planer.png",
+    flags = {},
+    subgroup = "intermediate-product",
+    order = "z[lol]-b[reverse-ratcheting-routing-planer]",
+    stack_size = 50,
+  },
+  }
---
+++ prototypes/recipes.lua
@@ -0,0 +1,25 @@
+data:extend{
+  {
+    type = "recipe",
+    name = "self-sealing-stem-bolt",
+    enabled = "true",
+    ingredients =
+    {
+      {"steel-plate", 5},
+      {"electronic-circuit", 2},
+    },
+    result="self-sealing-stem-bolt",
+  },
+  {
+    type = "recipe",
+    name = "reverse-ratcheting-routing-planer",
+    enabled = "true",
+    ingredients =
+    {
+      {"self-sealing-stem-bolt", 5},
+      {"iron-gear-wheel", 10},
+      {"advanced-circuit", 2}
+    },
+    result="reverse-ratcheting-routing-planer",
+  },
+}
